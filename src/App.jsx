b7762aac558f2db45a9211501be3c2b091e301e7
ProxxyX <shresthaprajjwol4@example.com>
2024-05-02 19:26:21 +0545

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Home />
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Footer from '../components/Footer'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <Footer />
+        </div>
     )
 }
 
